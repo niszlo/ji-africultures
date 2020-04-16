94a83e744512d8650e044587a3119f7395a551ef
Camilo Castro <camilo@ninjas.cl>
2020-04-16 15:04:16 -0400

    added uuid to env

--- xcode/Jasonette/Core/Jason/Jason.m
+++ xcode/Jasonette/Core/Jason/Jason.m
@@ -1983,6 +1983,7 @@
 
     CGRect bounds = [[UIScreen mainScreen] bounds];
     dict[@"device"] = @{
+            @"uuid": [[[UIDevice currentDevice] identifierForVendor] UUIDString],
             @"width": @(bounds.size.width),
             @"height": @(bounds.size.height),
             @"os": @{
