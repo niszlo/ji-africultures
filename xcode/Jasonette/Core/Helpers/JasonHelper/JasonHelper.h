c40682be11c341ff0881392ea70c449fcd3983a6
Camilo <camilo.castro.cabrera@gmail.com>
2020-04-20 16:21:43 -0400

    added NS_ASSUME_NONNULL to quiet warnings

--- xcode/Jasonette/Core/Helpers/JasonHelper/JasonHelper.h
+++ xcode/Jasonette/Core/Helpers/JasonHelper/JasonHelper.h
@@ -10,6 +10,7 @@
 #import "UICKeyChainStore.h"
 #import "JasonParser.h"
 @interface JasonHelper : NSObject
+NS_ASSUME_NONNULL_BEGIN
 + (NSDate *)dateWithISO8601String:(NSString *)dateString;
 + (NSDate *)dateFromString:(NSString *)dateString withFormat:(NSString *)dateFormat;
 + (MFMessageComposeViewController *)sendSMS:(NSString *)message to:(NSString *)phone;
@@ -49,4 +50,5 @@
 
 + (id)                              loadErrorJson;
 + (nullable NSDictionary *) hjson_to_json: (nonnull NSString *) content;
+NS_ASSUME_NONNULL_END
 @end
