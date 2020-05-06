7133a95c5b596f9a35a38d867195a5947e94efaf
Camilo Castro <camilo@ninjas.cl>
2020-05-06 11:45:16 -0400

    updated pods

--- xcode/Pods/AFNetworking/UIKit+AFNetworking/UIImage+AFNetworking.h
+++ xcode/Pods/AFNetworking/UIKit+AFNetworking/UIImage+AFNetworking.h
@@ -1,6 +1,8 @@
-// UIKit+AFNetworking.h
 //
-// Copyright (c) 2011–2016 Alamofire Software Foundation ( http://alamofire.org/ )
+//  UIImage+AFNetworking.h
+//  
+//
+//  Created by Paulo Ferreira on 08/07/15.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,25 +22,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <TargetConditionals.h>
-
-#ifndef _UIKIT_AFNETWORKING_
-    #define _UIKIT_AFNETWORKING_
-
 #if TARGET_OS_IOS || TARGET_OS_TV
-    #import "AFAutoPurgingImageCache.h"
-    #import "AFImageDownloader.h"
-    #import "UIActivityIndicatorView+AFNetworking.h"
-    #import "UIButton+AFNetworking.h"
-    #import "UIImage+AFNetworking.h"
-    #import "UIImageView+AFNetworking.h"
-    #import "UIProgressView+AFNetworking.h"
-#endif
 
-#if TARGET_OS_IOS
-    #import "AFNetworkActivityIndicatorManager.h"
-    #import "UIRefreshControl+AFNetworking.h"
-    #import "WKWebView+AFNetworking.h"
-#endif
+#import <UIKit/UIKit.h>
 
-#endif /* _UIKIT_AFNETWORKING_ */
+@interface UIImage (AFNetworking)
+
++ (UIImage *)safeImageWithData:(NSData *)data;
+
+@end
+
+#endif
